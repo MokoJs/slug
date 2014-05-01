c9384280945830bb4d9b19f7c6a25a9c67de8f1e
Ryan Schmukler <rschmukler@gmail.com>
2014-05-01 16:24:46 -0400

    add empty tests

---
+++ test/moko-slug-test.js
@@ -0,0 +1,22 @@
+require('co-mocha');
+
+var moko = require('moko'),
+    expect = require('expect.js'),
+    slug = requite('../');
+
+describe('moko-slug', function() {
+  describe('simple attributes', function() {
+    it('works on instantiation');
+    it('works on change');
+  });
+
+  describe('format strings', function() {
+    it('works on instantiation');
+    it('works on change');
+  });
+
+  describe('format functions', function() {
+    it('works on instantiation');
+    it('works on change');
+  });
+});
